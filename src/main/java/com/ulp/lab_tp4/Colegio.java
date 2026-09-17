0c5f6757ea9b78c523ca1216f88651b6ff93cc09
Andres Ferra <andres.ferra@mi.unc.edu.ar>
2026-09-17 20:09:46 -0300

    implementacion parcial de la clase Colegio

---
+++ src/main/java/com/ulp/lab_tp4/Colegio.java
@@ -0,0 +1,21 @@
+package com.ulp.lab_tp4;
+
+public class Colegio {
+
+    public static void main(String[] args) {
+        Materia web2 = new Materia(8, "Web 2", 2);
+        Materia matematica = new Materia(2, "Matemáticas", 1 );
+        Materia lab1 = new Materia(4, "Laboratorio 1", 1 );
+        Alumno martin = new Alumno(1001, "López", "Martin");
+        Alumno brenda = new Alumno(1002, "Martínez", "Brenda");
+        
+        martin.agregarMateria(web2);
+        martin.agregarMateria(matematica);
+        martin.agregarMateria(lab1);
+        brenda.agregarMateria(web2);
+        brenda.agregarMateria(matematica);
+        brenda.agregarMateria(lab1);
+        
+        brenda.agregarMateria(lab1);
+    }
+}
--- src/main/java/com/ulp/lab_tp4/Lab_TP4.java
+++
@@ -1,8 +0,0 @@
-package com.ulp.lab_tp4;
-
-public class Lab_TP4 {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
