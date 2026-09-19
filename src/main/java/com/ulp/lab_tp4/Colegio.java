56664b8fa1ee67221bf4c9c570532e681072dd0f
Santiago Vera <verasantiago937@gmail.com>
2026-09-18 21:22:21 -0300

    Eliminacion del metodo main de la clase ventana principal para que funcione el trabajo completo

--- src/main/java/com/ulp/lab_tp4/Colegio.java
+++ src/main/java/com/ulp/lab_tp4/Colegio.java
@@ -1,8 +1,24 @@
 package com.ulp.lab_tp4;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
 public class Colegio {
 
     public static void main(String[] args) {
+       //Solucion a la parte grafica luego de sacar el main del jframe ventanaprincipal//
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            java.util.logging.Logger.getLogger(Colegio.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        
+        
         Materia web2 = new Materia(8, "Web 2", 2);
         Materia matematica = new Materia(2, "Matemáticas", 1 );
         Materia lab1 = new Materia(4, "Laboratorio 1", 1 );
@@ -22,5 +38,12 @@ public class Colegio {
                 + martin.cantidadMaterias());
         System.out.println("Cantidad de materias Brenda: "
                 + brenda.cantidadMaterias());
+        
+        
+      java.awt.EventQueue.invokeLater(() -> {
+            new VentanaPrincipal().setVisible(true);
+        });  
+        
+        
     }
 }
